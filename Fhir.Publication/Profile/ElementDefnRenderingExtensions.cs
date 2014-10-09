55cc3dfcce2d3ea63e6e329affb6096341793136
Ewout Kramer <e.kramer@furore.com>
2014-10-09 17:33:05 +0200

    Added rendering for display for primitives and some basic datatypes

--- Fhir.Publication/Profile/ElementDefnRenderingExtensions.cs
+++ Fhir.Publication/Profile/ElementDefnRenderingExtensions.cs
@@ -31,7 +31,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Hl7.Fhir.Introspection;
 using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
 
 namespace Hl7.Fhir.Publication
 {
@@ -55,13 +57,84 @@ namespace Hl7.Fhir.Publication
             return String.Join(", ", ext.Context);
         }
 
-        public static string RenderValue(this Element value)
+        public static string ForDisplay(this CodeableConcept value)
         {
-            if (value is FhirUri) return ((FhirUri)value).Value;
-            if (value is FhirString) return ((FhirString)value).Value;
-            if (value is Code) return ((Code)value).Value;
+            var result = value.Text;
 
-            return "(todo)";
+            if (value.Coding != null && value.Coding.Count != 0)
+            {
+                if (!String.IsNullOrEmpty(result)) result += " ";
+                result += "{" + String.Join(", ", value.Coding.Select(cod => cod.ForDisplay())) + "}";
+            }
+
+            return result;
+        }
+
+        public static string ForDisplay(this Coding value)
+        {
+            var result = value.Code;
+
+            if(value.System != null) result += "@" + value.System;
+            if(value.Display != null) result += " (" + value.Display + ")";
+            return result;
+        }
+
+        public static string ForDisplay(this Identifier value)
+        {
+            var result = value.Value;
+
+            if(value.System != null) result += "@" + value.System;
+
+            return result;
+        }
+
+        public static string ForDisplay(this Period value)
+        {
+            return String.Format("[{0},{1}]", value.Start ?? "\u221E", value.End ?? "\u221E");
+        }
+
+
+        public static string ForDisplay(this Range value)
+        {
+            return String.Format("[{0},{1}]", value.Low == null ? "\u221E" : value.Low.ForDisplay(), 
+                value.High == null ? "\u221E" : value.High.ForDisplay());
+        }
+
+        public static string ForDisplay(this Quantity value)
+        {
+            string comp = value.Comparator==null ? String.Empty : value.Comparator.ConvertTo<string>();          
+            var val = value.Value == null ? "??" : value.Value.ConvertTo<string>();
+            var unit = value.Units != null ? (value.Units.Length > 2 ? " " : "") + value.Units : value.Code;
+
+            return comp+val+unit;
+        }
+
+        public static string ForDisplay(this Ratio value)
+        {
+            if (value.Numerator!=null && value.Denominator!=null)
+                return value.Numerator.ForDisplay() + " : " + value.Denominator.ForDisplay();
+            else
+                return String.Empty;
+        }
+
+        public static string ForDisplay(this Element value)
+        {
+            if (value is FhirBoolean) return ((FhirBoolean)value).Value.ConvertTo<string>();
+            if (value is Integer) return ((Integer)value).Value.ConvertTo<string>();
+            if (value is FhirDecimal) return ((FhirDecimal)value).Value.ConvertTo<string>();
+            if (value is Base64Binary) return Convert.ToBase64String(((Base64Binary)value).Value);
+            if (value is Instant) return ((Instant)value).Value.ConvertTo<string>();
+            if (value is FhirString) return ((FhirString)value).Value;            
+            if (value is FhirUri) return ((FhirUri)value).Value;
+            if (value is Date) return ((Date)value).Value;
+            if (value is FhirDateTime) return ((FhirDateTime)value).Value;
+                       
+            if (value is Code) return ((Code)value).Value;
+            if (value is Oid) return ((Oid)value).Value;
+            if (value is Uuid) return ((Uuid)value).Value;
+            if (value is Id) return ((Id)value).Value;
+
+            return String.Format("({0} todo)", value.GetType().Name);
         }
     }
 }
