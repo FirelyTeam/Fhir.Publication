069897b882959c6f23b4a6a09e77c645f6b3f0b3
Martijn Harthoorn <m.harthoorn@furore.com>
2014-10-13 13:19:11 +0200

    Processor to new file

--- Fhir.Publication/Framework/IProcessor.cs
+++ Fhir.Publication/Framework/IProcessor.cs
@@ -11,42 +11,7 @@ namespace Hl7.Fhir.Publication
         void Process(Document input, Stage output);
     }
 
-    public static class Processor
-    {
-        
-        public static Stage Process(this IProcessor processor, Stage input)
-        {
-            Stage output = Stage.New();
-
-            foreach (Document document in input.Documents)
-            {
-                processor.Process(document, output);
-            }
-            return output;
-        }
-
-        public static Stage Process(this PipeLine pipeline, Stage input)
-        {
-            Stage output = input;
-            foreach (IProcessor p in pipeline.Processors)
-            {
-                output = p.Process(output);
-            }
-            return output;
-        }
-
-        public static Stage ToStage(this IFilter filter)
-        {
-            Stage stage = new Stage(filter.GetItems());
-            return stage;
-        }
-
-        public static Stage Process(this PipeLine pipeline, IFilter filter)
-        {
-            Stage stage = filter.ToStage();
-            return Processor.Process(pipeline, stage);
-        }
-    }
+    
 }
 
 
---
+++ Fhir.Publication/Framework/Processor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Publication.Framework
+{
+    public static class Processor
+    {
+
+        public static Stage Process(this IProcessor processor, Stage input)
+        {
+            Stage output = Stage.New();
+
+            foreach (Document document in input.Documents)
+            {
+                processor.Process(document, output);
+            }
+            return output;
+        }
+
+        public static Stage Process(this PipeLine pipeline, Stage input)
+        {
+            Stage output = input;
+            foreach (IProcessor p in pipeline.Processors)
+            {
+                output = p.Process(output);
+            }
+            return output;
+        }
+
+        public static Stage ToStage(this IFilter filter)
+        {
+            Stage stage = new Stage(filter.GetItems());
+            return stage;
+        }
+
+        public static Stage Process(this PipeLine pipeline, IFilter filter)
+        {
+            Stage stage = filter.ToStage();
+            return Processor.Process(pipeline, stage);
+        }
+    }
+}
